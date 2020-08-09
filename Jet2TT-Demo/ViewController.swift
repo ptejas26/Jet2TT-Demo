e4c35ae6914d1e844c2336ef526b1ef9b963f12f
Tejas Patelia <tejas.patelia@globallogic.com>
2020-08-09 19:35:18 +0530

    Initial Commit

---
+++ Jet2TT-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jet2TT-Demo
+//
+//  Created by GlobalLogic on 09/08/20.
+//  Copyright © 2020 Tejas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Jet2TT-DemoTests/Jet2TT_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Jet2TT_DemoTests.swift
+//  Jet2TT-DemoTests
+//
+//  Created by GlobalLogic on 09/08/20.
+//  Copyright © 2020 Tejas. All rights reserved.
+//
+
+import XCTest
+@testable import Jet2TT_Demo
+
+class Jet2TT_DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
