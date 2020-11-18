8e000e9fcaa22f2719fc69a829aeeee5e3448884
Tejas <ptejas.patelia@gmail.com>
2020-11-18 17:35:49 +0530

    Making minor change to git

--- Jet2TT-Demo/AppDelegate.swift
+++ Jet2TT-Demo/AppDelegate.swift
@@ -12,7 +12,7 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // 1 This change is from Tejas
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
